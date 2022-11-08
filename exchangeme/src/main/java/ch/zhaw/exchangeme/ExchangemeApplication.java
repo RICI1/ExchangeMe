77ddc40ba71ef974ed631e4fa096c49fe866864a
Ricardo <sequeric@students.zhaw.ch>
2022-11-08 21:34:27 +0100

    projectsetup

---
+++ exchangeme/src/main/java/ch/zhaw/exchangeme/ExchangemeApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.exchangeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangemeApplication.class, args);
+	}
+
+}
---
+++ exchangeme/src/test/java/ch/zhaw/exchangeme/ExchangemeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.exchangeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
